36a853e587a97c9b49898afc2673e9bbeccce01e
awildings <nicholas.frederich.lagaunne@gmail.com>
2017-07-24 21:57:27 +0700

    refactor function

--- src/isEmpty.js
+++ src/isEmpty.js
@@ -1,33 +1,34 @@
 // @flow
 
 function isEmpty(param: mixed): boolean {
-  const getParamType = typeof param;
+  const getParamType: string = typeof param;
   if (isNaN(param) && getParamType !== 'object') {
     return false;
   } else if (param === null) {
     return false;
-  } else if (getParamType === 'string' || getParamType === 'boolean' || getParamType === 'number') {
-    if (param === 0) {
-      return false;
-    } else if (param === false) {
-      return false;
-    } else if (param === ' ') {
-      return false;
-    }
-  } else if (getParamType === 'object') {
-    if (Array.isArray(param) /* true*/ && param[0] === '') {
-      return false;
-    } else if (
-      Array.isArray(param) /* true*/ &&
-      Array.isArray(param[0]) &&
-      param[0][0] === undefined
-    ) {
-      return false;
-    } else if (Object.is(param, param) && Object.values(param).length !== 0) {
-      return false;
-    }
   }
-  // if type not defined by above code
+  switch (getParamType) {
+    case 'string':
+      if (param === ' ') return false;
+      break;
+    case 'boolean':
+      if (param === false) return false;
+      break;
+    case 'number':
+      if (param === 0) return false;
+      break;
+    case 'object':
+      if (Array.isArray(param) && param[0] === '') {
+        return false;
+      } else if (Array.isArray(param) && Array.isArray(param[0]) && param[0][0] === undefined) {
+        return false;
+      } else if (Object.is(param, param) && Object.values(param).length !== 0) {
+        return false;
+      }
+      break;
+    default:
+      break;
+  }
   return true;
 }
 
